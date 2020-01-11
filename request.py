514fd010850cd8c8897d56e1796ec397d911824d
Jonathan <jonathan_silva02@hotmail.com>
2020-01-11 13:47:52 -0300

    Removido função CSV

--- request.py
+++ request.py
@@ -14,7 +14,7 @@ from xlsxwriter.workbook import Workbook
 #realiza o login
 def init_session(login_url):
     with requests.session() as session_requests:
-        login_url = "https://redmine.intranet.gertec/login?back_url=http%3A%2F%2F172.18.1.26%2F"
+        login_url = "Link"
         result = session_requests.get(login_url, verify = False)
         data_page = html.fromstring(result.text)
     #captura o token da pagina
@@ -32,14 +32,14 @@ def gerar_dados_login(session_requests, authenticity_token, login_url):
     print(payload["authenticity_token"])
     login(session_requests, payload, login_url)
 
-def login(session_requests, payload, login_url= "https://redmine.intranet.gertec/login?back_url=http%3A%2F%2F172.18.1.26%2F" ):
+def login(session_requests, payload, login_url= "LINK" ):
 #realiza login
     post = session_requests.post(login_url, data=payload, headers = dict(referer=login_url))
     print(post.status_code)
     downlaod_csv(session_requests)
 
-def  downlaod_csv(session_requests, url_csv = "https://redmine.intranet.gertec/issues.csv" ):
-    url_csv = "https://redmine.intranet.gertec/issues.csv?c%5B%5D=project&c%5B%5D=tracker&c%5B%5D=status&c%5B%5D=subject&c%5B%5D=assigned_to&c%5B%5D=updated_on&c%5B%5D=priority&c%5B%5D=fixed_version&c%5B%5D=estimated_hours&c%5B%5D=cf_22&f%5B%5D=status_id&f%5B%5D=cf_22&f%5B%5D=&group_by=&op%5Bcf_22%5D=%2A&op%5Bstatus_id%5D=o&set_filter=1&t%5B%5D=estimated_hours&t%5B%5D=spent_hours&utf8=%E2%9C%93"
+def  downlaod_csv(session_requests, url_csv = "LINK" ):
+    url_csv = "LINK"
 
     #result2 = session_requests.get(url, headers=dict(referer=url))
     #print(result2.text)
@@ -65,22 +65,7 @@ def  downlaod_csv(session_requests, url_csv = "https://redmine.intranet.gertec/i
                 line_count += 1
         print(f'Processed {line_count} lines.')
 
-#converter_csv_para_xls(nome_arquivo)
 
-
-''''
-def converter_csv_para_xls(nome_arquivo):
-    for csvfile in glob.glob(os.path.join('C:\\Users\\joferreira\\PycharmProjects\\Get_Tickets', '*.csv')):
-        workbook = Workbook(csvfile[:-4] + '.xlsx')
-        worksheet = workbook.add_worksheet()
-        with open(nome_arquivo, 'rt', encoding='ascii') as f:
-            reader = csv.reader(f)
-            for r, row in enumerate(reader):
-                for c, col in enumerate(row):
-                    worksheet.write(r, c, col)
-        workbook.close()
-'''
-
-init_session("https://redmine.intranet.gertec/login?back_url=http%3A%2F%2F172.18.1.26%2F")
+init_session("Link")
 
 
